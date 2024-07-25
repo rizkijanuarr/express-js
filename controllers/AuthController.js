77fdf988fa6ba2e2fcd59a6226aafb55f0ffc7b0
rizkijanuarr <72480809+rizkijanuarr@users.noreply.github.com>
2024-07-25 21:42:42 +0700

    memperbaiki security untuk daftar agar lebih safe dan memperbaiki responses msg eror

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,17 +2,16 @@ const { User } = require("../models");
 
 exports.registerUser = async (req, res) => {
   try {
-    let { name, email, password, passwordConfirm } = req.body;
-
-    if (password != passwordConfirm) {
+    if (req.body.password != req.body.passwordConfirm) {
       return res.status(400).json({
-        message: "Password dan PasswordConfirm Tidak Sama!",
+        message: "Fail",
+        error: ["Password dan Password Confirm Tidak Sama!"],
       });
     } else {
       const newUser = await User.create({
-        name,
-        email,
-        password,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
       });
       return res.status(201).json({
         message: "Berhasil registrasi!",
@@ -22,7 +21,7 @@ exports.registerUser = async (req, res) => {
   } catch (error) {
     return res.status(400).json({
       message: "Validasi Error",
-      error: error.errors,
+      error: error.errors.map((err) => err.message),
     });
   }
 };
